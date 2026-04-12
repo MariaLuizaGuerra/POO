d5db46ae60551bf5f46f4395329d1006b7319d40
MariaLuizaGuerra <marialuizaguerrapedroso@gmail.com>
2026-04-12 19:32:23 -0300

    1204

--- Lista1_Poo/src/controleEstoque/Produto.java
+++ Lista1_Poo/src/controleEstoque/Produto.java
@@ -5,24 +5,37 @@ public class Produto {
 	private String nome;
 	private int quantidade;
 	private double preco;
-	
-	public Produto(String nome, int quantidade, float preco) {
-		
+
+	public Produto(String nome, int quantidade, double string) {
+
 		this.nome = nome;
 		this.quantidade = quantidade;
-		this.preco = preco;
-		
+		this.preco = string;
+
 	}
-	
+
 	public void adicionarEstoque(int qtd) {
 		quantidade += qtd;
 	}
-	
-	public void retirarEstoque(int qtd) {
-		quantidade -= qtd;
+
+	public void retirarEstoque(int qtde) {
+	    if ((quantidade - qtde) < 0) {
+	    	System.err.println("Erro: Quantidade insuficiente em estoque para retirar " + qtde + " unidades.");
+	    } 
+	    else {
+	        quantidade -= qtde;
+	    }
 	}
-	
+
 	public void aplicarDesconto(double porcentagem) {
 		preco = preco - ((preco * porcentagem) / 100);
+	}
+
+	public void exibirInformacoes() {
+		System.out.println("-----------INFORMAÇÕES------------");
+		System.out.println("Nome: " + nome);
+		System.out.println("Quantidade: " + quantidade);
+		System.out.println("Preco unitario: " + preco);
+		System.out.println("Valor total em estoque: " + (quantidade * preco));
 	}
 }
--- Lista1_Poo/src/controleEstoque/ProgramaPrincipal.java
+++ Lista1_Poo/src/controleEstoque/ProgramaPrincipal.java
@@ -3,8 +3,17 @@ package controleEstoque;
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Produto produto1 = new Produto("Notebook", 10, 5000);
+		produto1.adicionarEstoque(10);
+		produto1.aplicarDesconto(10);	
+		produto1.exibirInformacoes();
+		
+		Produto produto2 = new Produto("PC DESKTOP", 3, 3200);
+		produto2.adicionarEstoque(2);
+		produto2.retirarEstoque(10);
+		produto2.exibirInformacoes();
+		
 	}
 
 }
